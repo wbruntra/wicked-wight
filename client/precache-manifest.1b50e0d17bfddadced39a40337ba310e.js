7e694b04170934399b108e0504b18bc0ee6ef8f2
William Bruntrager <w.bruntrager@gmail.com>
2020-04-16 11:31:29 +0200

    Updates

---
+++ client/precache-manifest.1b50e0d17bfddadced39a40337ba310e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a1a91960423c089ad5e",
+    "url": "/static/css/main.46436fa2.chunk.css"
+  },
+  {
+    "revision": "4a1a91960423c089ad5e",
+    "url": "/static/js/main.efcef6de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4ae312e863eb4b9d913a",
+    "url": "/static/js/2.c4d998d8.chunk.js"
+  },
+  {
+    "revision": "c6b234719965cc10df0f8d12c1f438dd",
+    "url": "/static/media/clippy.c6b23471.svg"
+  },
+  {
+    "revision": "9a1d6a1e6445f750c31c17ca42da7665",
+    "url": "/index.html"
+  }
+];
--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/app/precache-manifest.c159ed223c0295e31943ac4dab188673.js"
+  "/precache-manifest.1b50e0d17bfddadced39a40337ba310e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
