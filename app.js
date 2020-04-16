6e0b6cb95e3e39e047f9ed873d82812d307076e7
William Bruntrager <w.bruntrager@gmail.com>
2020-04-16 11:26:31 +0200

    Updates

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+const apiRoutes = require('./routes/api');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'client','build')));
+
+app.use('/api', apiRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client','build','index.html'))
+  })
+}
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('ERROR');
+});
+
+module.exports = app;
